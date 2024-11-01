6edb7d18a11bdc556510246d73c032ab2d45dd75
JanoTheDev <64414083+JanoTheDev@users.noreply.github.com>
2024-11-01 09:22:38 +0200

    Addition of new icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,30 @@
 "use client";
 
-import Navbar, { AppsAtoms } from "../components/navbar";
-import { useAtom } from "jotai";
+import React from "react";
+import Navbar from "../components/navbar";
 import NotePad from "@/components/notepad";
+import FileExplorer from "@/components/file-explorer";
+import RecycleBin from "@/components/recycle-bin";
+import Folder from "@/components/folder";
+import { useAtom } from "jotai";
+import { AppsAtoms } from "@/components/navbar";
+import FileExplore from "@/components/file explorer";
 
 export default function Home() {
-  // const [apps, setApps] = useAtom(AppsAtoms);
+  // const [apps] = useAtom(AppsAtoms);
+
   return (
     <div className="h-screen w-screen overflow-hidden relative">
       <div className="flex flex-col gap-3 m-2 absolute inset-0">
-        <NotePad id={1} />
-        <NotePad id={2}/>
+        <FileExplore id={1} name="Desktop" />
+        <RecycleBin id={2} />
+        <Folder id={3} name="Documents" />
+        <Folder id={4} name="Downloads" />
+        <Folder id={5} name="Pictures" />
+        <NotePad id={6} />
+        <NotePad id={7} />
       </div>
-      
+
       <div className="absolute bottom-0 left-0 right-0">
         <Navbar />
       </div>
